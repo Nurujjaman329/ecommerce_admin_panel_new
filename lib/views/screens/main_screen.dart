6848e0f8f811a43972a3c11b35b1caf3088a6e79
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-03-19 12:41:21 +0600

    add header & footer

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -64,10 +64,6 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return AdminScaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.yellow.shade900,
-        title: Text('Management'),
-      ),
       sideBar: SideBar(
         items: [
           AdminMenuItem(
@@ -110,6 +106,32 @@ class _MainScreenState extends State<MainScreen> {
         onSelected: (item) {
           screenSelector(item);
         },
+        header: Container(
+          height: 50,
+          width: double.infinity,
+          color: const Color(0xff444444),
+          child: const Center(
+            child: Text(
+              'Header',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        footer: Container(
+          height: 50,
+          width: double.infinity,
+          color: const Color(0xff444444),
+          child: const Center(
+            child: Text(
+              'Footer',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
       body: _selectedItem,
     );
