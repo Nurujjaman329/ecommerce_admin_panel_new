d38e5c1fd7b3465397e4661a7765d05af43f7608
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-03-19 13:48:18 +0600

    banner page design

--- lib/views/screens/side_bar_screens/upload_banner_screens.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screens.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UploadBannerScreens extends StatefulWidget {
- static const String routeName = '\UploadBannerScreens';
+  static const String routeName = '\UploadBannerScreens';
 
   @override
   State<UploadBannerScreens> createState() => _UploadBannerScreensState();
@@ -11,16 +11,93 @@ class _UploadBannerScreensState extends State<UploadBannerScreens> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: EdgeInsets.all(10.0),
-        child: Text(
-          'Upload Banner',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Banners',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(
+                          color: Colors.grey.shade800,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Text('Banners'),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * .040,
+                          width: MediaQuery.of(context).size.width * .10,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              5.0,
+                            ),
+                            color: Colors.yellow.shade900,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Upload Image',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: MediaQuery.of(context).size.height * .040,
+                  width: MediaQuery.of(context).size.width * .050,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      5.0,
+                    ),
+                    color: Colors.yellow.shade900,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
